dc72a9ee21fd031de3d59ca3dee4403c6d8f37d7
Matt Nicholson <matthewn1991@gmail.com>
2024-10-05 06:45:19 -0400

    more cleanup

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -3,6 +3,7 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import noRelativeImportPaths from "eslint-plugin-no-relative-import-paths";
 
 export default tseslint.config(
   { ignores: ["dist"] },
@@ -16,12 +17,17 @@ export default tseslint.config(
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
+      "no-relative-import-paths": noRelativeImportPaths,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
+      ],
+      "no-relative-import-paths/no-relative-import-paths": [
+        "warn",
+        { allowSameFolder: true, rootDir: "src", prefix: "" },
       ],
     },
   },
