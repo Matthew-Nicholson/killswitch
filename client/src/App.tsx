46e6aed7b899f55a94bdca2fb004e26b861b9669
unknown <destinygamingblogs@gmail.com>
2024-10-05 23:10:15 -0400

    Added table to databse and query test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -20,7 +20,7 @@ function App() {
       <h1>{import.meta.env.VITE_HELLO}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count is {count} candy
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
