1caa5a04cd40fe62e52444546851190ec3b099d5
Matt Nicholson <matthewn1991@gmail.com>
2024-10-05 23:14:21 -0400

    nextui+tailwind

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { App } from "./App.tsx";
+import { NextUIProvider } from "@nextui-org/react";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <NextUIProvider>
+      <main className="">
+        <App />
+      </main>
+    </NextUIProvider>
   </StrictMode>,
 );
