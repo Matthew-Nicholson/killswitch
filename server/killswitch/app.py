51480119cd68b2669a1ea35e10b371a23f3a6a83
unknown <destinygamingblogs@gmail.com>
2024-10-10 00:56:11 -0400

    Moved logic to it's own file and updated get and put methods to allow feature to be retrieved through an id rather than name

--- server/killswitch/app.py
+++ server/killswitch/app.py
@@ -2,50 +2,21 @@ from datetime import datetime
 from flask import Flask, jsonify, render_template, request
 from redis import Redis
 from flask_cors import CORS
+from helpers import insert_test_data,create_new_table,insert_feature_to_db, has_feature_been_deleted, convert_enabled_value  # Import the helper functions
 import sqlite3
-
-db = "feature_flag.db"
-con = sqlite3.connect(db)
-cur = con.cursor()
-
-# con.execute("DROP TABLE flag") 
-
-create_table_query = '''
-CREATE TABLE  IF NOT EXISTS "flag" (
-    "id" INTEGER NOT NULL UNIQUE,
-    "name" TEXT NOT NULL,
-    "description" TEXT,
-    "enabled" INTEGER,
-    "roll_out" INTEGER,
-    "deleted" INTEGER,
-    "deleted_at" INTEGER,
-    "created_at" INTEGER NOT NULL,
-    "updated_at" INTEGER NOT NULL,
-    PRIMARY KEY("id" AUTOINCREMENT),
-    CHECK("roll_out" <= 100 AND "roll_out" >= 0),
-    CHECK("deleted" <= 1 AND "deleted" >= 0)
-);
-'''
-cur.execute(create_table_query)
 
 insert_query = '''
 INSERT INTO "flag" (name, description, roll_out, enabled, deleted, deleted_at, created_at, updated_at)
 VALUES ( ?, ?, ?, ?, ?, ?, ?, ?);
 '''
-# data_to_insert = ('New Feature', 'This feature enables X.', 75, 1, 0,0, int(datetime.now().timestamp()), int(datetime.now().timestamp()))
-# cur.execute(insert_query, data_to_insert)
-con.commit()
-con.close()
+db = "feature_flag.db"
 
-def insert_feature_to_db(name,description,roll_out, enabled):
-    global db
-    global insert_query
-    con = sqlite3.connect(db)
-    cur = con.cursor()
-    data_to_insert = (name, description, roll_out, enabled, 0,0, int(datetime.now().timestamp()), int(datetime.now().timestamp()))
-    cur.execute(insert_query, data_to_insert)
-    con.commit()
-    con.close()
+create_new_table(db)
+
+def does_have_key(pattern):
+    for _ in redis.scan_iter(match=pattern):
+        return True
+    return False
 
 app = Flask(__name__)
 CORS(
@@ -72,27 +43,33 @@ def index():
         redis.set("planet", planet)
     return render_template("index.html", planet=redis.get("planet").decode("utf-8"))
 
-def has_feature_been_deleted(feature):
-    return int(feature['deleted']) == 1   
-
-def convert_enabled_value(feature):
-    return 1 if feature['enabled']is True else 0
-
-
 @app.route("/features", methods=["POST"])
 def feature():
     json = request.get_json()
-    name = json["name"]
+    title = json["title"]
     description= json["description"]
     roll_out = json["roll_out"]
     enabled = convert_enabled_value(json)
-
-    redis_key = f"feature:{name}"
-    if redis.exists(redis_key):
-        return f"Feature '{name}' already exists. Choose a different name."
     
-    insert_feature_to_db(name,description,roll_out,enabled)
+    insert_feature_to_db(db, title,description,roll_out,enabled)
+    with sqlite3.connect(db) as conn:
+            cur = conn.cursor()
+            cur.execute(f'''
+            SELECT id 
+            FROM flag
+            WHERE name = '{title}'
+            ''')
+            result = cur.fetchone()
+            if result:
+                id = result[0]
+                print(f"ID for feature '{title}': {id}")
+            else:
+                print(f"No feature found with name '{title}'")
+    redis_key = f"feature:{id}"
+
     redis.hset(redis_key, mapping= {
+        "id": id,
+        "title": title,
         "enabled": enabled,
         "description": description,
         "roll_out": roll_out,
@@ -104,31 +81,43 @@ def feature():
     
     return "Feature Created"
 
-@app.route("/feature/<name>", methods=["GET"])
-def get_feature(name):
-    redis_key =f"feature:{name}"
+@app.route("/feature/<id>", methods=["GET"])
+def get_feature(id):
+    redis_key =f"feature:{id}"
     feature_data = redis.hgetall(redis_key)
     if feature_data:
         if has_feature_been_deleted(feature_data):
-             return jsonify({"error": f"Feature '{name}' not found"}), 404
-        return jsonify({"name":name, "data":feature_data}), 200
+             return jsonify({"error": f"Feature '{id}' not found"}), 404
+        return jsonify({"name":id, "data":feature_data}), 200
     else:
-        return jsonify({"error": f"Feature '{name}' not found"}), 404
+        return jsonify({"error": f"Feature '{id}' not found"}), 404
   
 @app.route("/features", methods=["GET"])
 def get_all_features():
     features = {}
+    if(does_have_key("feature:*") == False): 
+        return features
+    try:
+        with sqlite3.connect(db) as conn:
+            cur = conn.cursor()
+            cur.execute('SELECT * FROM flag')
+            rows = cur.fetchall()
+            for row in rows:
+                print(row)
+    except sqlite3.Error as e:
+        print(e)
+
     for key in redis.scan_iter(match="feature:*"):
         feature_data = redis.hgetall(key)
         if(has_feature_been_deleted(feature_data) == False):
             features[key] = feature_data
     return features
 
-@app.route("/feature/<name>", methods=["PUT"])
-def update_feature(name):
-    redis_key =f"feature:{name}"
+@app.route("/feature/<id>", methods=["PUT"])
+def update_feature(id):
+    redis_key =f"feature:{id}"
     if not redis.exists(redis_key):
-        return jsonify({"error": f"Feature '{name}' not found"}), 404
+        return jsonify({"error": f"Feature '{id}' not found"}), 404
     
     update_data = request.get_json()
 
@@ -139,7 +128,7 @@ def update_feature(name):
 
     redis.hset(redis_key, mapping=update_data)
     redis.hset(redis_key, "updated_at", int(datetime.now().timestamp()))
-    return jsonify({"message": f"Feature '{name}' updated successfully", "updated_fields": update_data}), 200
+    return jsonify({"message": f"Feature '{id}' updated successfully", "updated_fields": update_data}), 200
 
 @app.route("/feature/<name>", methods=["DELETE"])
 def delete_feature(name):
@@ -155,15 +144,11 @@ def delete_feature(name):
     })
     return jsonify({"message": f"Feature '{name}' deleted successfully"}), 200
 
-
-
-
 @app.route("/count", methods=["GET"])
 def count():
     print("yey running")
     response = {"count": 1}
     return jsonify(response)
-
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
---
+++ server/killswitch/helpers.py
@@ -0,0 +1,127 @@
+# helpers.py
+import sqlite3
+from datetime import datetime
+from redis import Redis
+insert_query = '''
+INSERT INTO "flag" (name, description,enabled,roll_out, deleted, deleted_at, created_at, updated_at)
+VALUES ( ?, ?, ?, ?, ?, ?, ?, ?);
+'''
+
+redis = Redis(host="127.0.0.1", port=6379,decode_responses=True)
+
+def has_feature_been_deleted(feature):
+    """
+    Check if a feature has been deleted based on the 'deleted' field in the feature hash.
+    Returns True if the feature has been marked as deleted.
+    """
+    return int(feature['deleted']) == 1   
+
+def convert_enabled_value(feature):
+    """
+    Convert the 'enabled' value in the feature hash to an integer (1 or 0).
+    Returns 1 if the feature is enabled, otherwise returns 0.
+    """
+    return 1 if feature['enabled'] is True else 0
+
+def create_new_table(db, should_drop=False, should_insert_test_data=False):
+    """
+    Creates a new table if needed.
+    """
+
+    con = sqlite3.connect(db)
+    cur = con.cursor()
+    if(should_drop):
+        con.execute("DROP TABLE flag") 
+        redis.flushdb()
+    create_table_query = '''
+    CREATE TABLE  IF NOT EXISTS "flag" (
+        "id" INTEGER NOT NULL UNIQUE,
+        "name" TEXT NOT NULL,
+        "description" TEXT,
+        "enabled" INTEGER,
+        "roll_out" INTEGER,
+        "deleted" INTEGER,
+        "deleted_at" INTEGER,
+        "created_at" INTEGER NOT NULL,
+        "updated_at" INTEGER NOT NULL,
+        PRIMARY KEY("id" AUTOINCREMENT),
+        CHECK("roll_out" <= 100 AND "roll_out" >= 0),
+        CHECK("deleted" <= 1 AND "deleted" >= 0)
+    );
+    '''
+    cur.execute(create_table_query)
+    
+    if(should_insert_test_data):
+        insert_test_data(db)
+
+def insert_test_data(db):
+    insert_feature_to_db(db,'New Feature','This feature enables X.',True,75)
+
+def add_feature_to_redis(db, feature):
+    id = None
+    title=feature['title']
+    description=feature['description']
+    enabled=feature['enabled']
+    roll_out=feature['roll_out']
+    with sqlite3.connect(db) as conn:
+            cur = conn.cursor()
+            cur.execute(f'''
+            SELECT id 
+            FROM flag
+            WHERE name = '{title}'
+            ''')
+            result = cur.fetchone()
+            if result:
+                id = result[0]
+                print(f"ID for feature '{title}': {id}")
+            else:
+                print(f"No feature found with name '{title}'")
+    redis_key = f"feature:{id}"
+        
+       
+    redis.hset(redis_key, mapping= {
+        "id": id,
+        "title": title,
+        "description": description,
+        "enabled": enabled,
+        "roll_out": roll_out,
+        "deleted": 0,
+        "deleted_at": 0,
+        "created_at": int(datetime.now().timestamp()),
+        "updated_at": int(datetime.now().timestamp())
+    })
+    
+def insert_feature_to_db(db, title,description,enabled,roll_out):
+    con = sqlite3.connect(db)
+    cur = con.cursor()
+    data_to_insert = {
+    "title": title,
+    "description": description,
+    "enabled":  enabled,
+    "roll_out": roll_out,
+    "deleted": 0,
+    "deleted_at": 0,
+    "created_at": int(datetime.now().timestamp()),
+    "updated_at": int(datetime.now().timestamp())
+    }
+
+    data_to_insert['enabled'] = convert_enabled_value(data_to_insert)
+
+    feature = (
+        data_to_insert["title"], 
+        data_to_insert["description"],
+        data_to_insert["enabled"],
+        data_to_insert['roll_out'],
+        data_to_insert['deleted'],
+        data_to_insert['deleted_at'],
+        data_to_insert['created_at'],
+        data_to_insert['updated_at']
+        )
+    
+    cur.execute(insert_query, feature)
+    con.commit()
+    con.close()
+
+    add_feature_to_redis(db,data_to_insert)
+
+    return "Feature Created"
